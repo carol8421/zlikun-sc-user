4746aa5dc83891c75325b6ab2159bdfa7be3431d
zlikun <zlikun-dev@hotmail.com>
2018-07-25 21:37:23 +0800

    注册中心开户认证时的配置
    http://cloud.spring.io/spring-cloud-static/Finchley.RELEASE/multi/multi__service_discovery_eureka_clients.html#_authenticating_with_the_eureka_server

--- src/main/java/com/zlikun/sc/UserApplication.java
+++ src/main/java/com/zlikun/sc/UserApplication.java
@@ -2,10 +2,13 @@ package com.zlikun.sc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+// @SpringCloudApplication 包含上面两个注解，还包含Hystrix组件，这里暂未使用
+// @SpringCloudApplication
 public class UserApplication {
 
     public static void main(String[] args) {

cf4cc327a137d8b06f45e69f99f189e3cff3da2e
zlikun <zlikun-dev@hotmail.com>
2018-07-25 21:56:58 +0800

    11.8 Using the EurekaClient
    http://cloud.spring.io/spring-cloud-static/Finchley.RELEASE/multi/multi__service_discovery_eureka_clients.html#_using_the_eurekaclient
    failure: No matches for the virtual host name :STORES

--- src/test/java/com/zlikun/sc/UserApplicationTests.java
+++ src/test/java/com/zlikun/sc/UserApplicationTests.java
@@ -1,6 +1,5 @@
 package com.zlikun.sc;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -9,8 +8,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class UserApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+//    @Autowired
+//    private EurekaClient discoveryClient;
+//
+//    @Test
+//    public void serviceUrl() {
+//        // http://cloud.spring.io/spring-cloud-static/Finchley.RELEASE/multi/multi__service_discovery_eureka_clients.html#_using_the_eurekaclient
+//        InstanceInfo instance = discoveryClient.getNextServerFromEureka("STORES", false);
+//        System.out.println(instance.getHomePageUrl());
+//    }
 
 }
